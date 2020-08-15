26bcbe46a22b7ed8c458dc3ad3b88c7818313cb7
shawn <q-x64@live.com>
2020-08-15 16:47:19 +0800

    fix model clear cache by update field only error

--- qx_base/qx_rest/models.py
+++ qx_base/qx_rest/models.py
@@ -46,6 +46,7 @@ class RestModelMixin(models.Model):
                 "field_name": "user_id" // or baby__user_id
             }
         },
+        "reload_data": False # reload data from db
     }
     '''
 
@@ -62,12 +63,12 @@ class RestModelMixin(models.Model):
         else:
             return getattr(ins, only_field_lst[index])
 
-    def _get_action(self, cls: "class", val: dict):
+    def _get_action(self, ins, cls: "class", val: dict):
         list_action = val.get('list_action', [])
         retrieve_action = val.get('retrieve_action', [])
         if val.get("field_only"):
             only_field_lst = val['field_name'].split("__")
-            _id = str(self._get_relate_field(self, only_field_lst))
+            _id = str(self._get_relate_field(ins, only_field_lst))
             list_action = [
                 item + ":" + _id
                 for item in list_action
@@ -78,8 +79,8 @@ class RestModelMixin(models.Model):
             ]
         return list_action, retrieve_action
 
-    def _foreign_clear_cache(self, cls: "class", val: dict):
-        list_action, retrieve_action = self._get_action(cls, val)
+    def _foreign_clear_cache(self, ins, cls: "class", val: dict):
+        list_action, retrieve_action = self._get_action(ins, cls, val)
         if val.get('foreign_set'):
             obj_ids = getattr(self, val['foreign_set']).all().values_list(
                 val.get("retrieve_field", 'id'), flat=True)
@@ -96,14 +97,14 @@ class RestModelMixin(models.Model):
                         cls, item, _id)
         return _func
 
-    def _custom_clear_cache(self, key, args):
+    def _custom_clear_cache(self, ins, key, args):
         _args = []
         for arg in args:
             if arg == '*':
                 _args.append('*')
             else:
                 arg_field_lst = arg.split("__")
-                _id = str(self._get_relate_field(self, arg_field_lst))
+                _id = str(self._get_relate_field(ins, arg_field_lst))
                 _args.append(_id)
 
         def _func():
@@ -111,8 +112,8 @@ class RestModelMixin(models.Model):
             RedisClient().clear_by_pattern(item)
         return _func
 
-    def _default_clear_cache(self, cls: "class", val: dict):
-        list_action, retrieve_action = self._get_action(cls, val)
+    def _default_clear_cache(self, ins, cls: "class", val: dict):
+        list_action, retrieve_action = self._get_action(ins, cls, val)
         if retrieve_action:
             _id = getattr(self, val.get("retrieve_field", 'id'))
         else:
@@ -129,26 +130,38 @@ class RestModelMixin(models.Model):
 
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
+
+        objs = [self]
+        if self.REST_CACHE_CLASS:
+            if self.REST_CACHE_CLASS.get('reload_data', False) and self.id:
+                origin = self.__class__.objects.get(id=self.id)
+                objs.append(origin)
+
         super().save(force_insert, force_update, using,
                      update_fields)
 
         if self.REST_CACHE_CLASS:
-            for cls, val in self.REST_CACHE_CLASS.get('default', {}).items():
-                self._default_clear_cache(cls, val)()
-            for cls, val in self.REST_CACHE_CLASS.get('foreign', {}).items():
-                self._foreign_clear_cache(cls, val)()
-            for cls, val in self.REST_CACHE_CLASS.get('custom', {}).items():
-                self._custom_clear_cache(cls, val)()
+            for ins in objs:
+                for cls, val in self.REST_CACHE_CLASS.get(
+                        'default', {}).items():
+                    self._default_clear_cache(ins, cls, val)()
+                for cls, val in self.REST_CACHE_CLASS.get(
+                        'foreign', {}).items():
+                    self._foreign_clear_cache(ins, cls, val)()
+                for cls, val in self.REST_CACHE_CLASS.get(
+                        'custom', {}).items():
+                    self._custom_clear_cache(ins, cls, val)()
 
     def delete(self, using=None, keep_parents=False):
         cache_func = []
         if self.REST_CACHE_CLASS:
+            ins = self
             for cls, val in self.REST_CACHE_CLASS.get('default', {}).items():
-                cache_func.append(self._default_clear_cache(cls, val))
+                cache_func.append(self._default_clear_cache(ins, cls, val))
             for cls, val in self.REST_CACHE_CLASS.get('foreign', {}).items():
-                self._foreign_clear_cache(cls, val)()
+                self._foreign_clear_cache(ins, cls, val)()
             for cls, val in self.REST_CACHE_CLASS.get('custom', {}).items():
-                cache_func.append(self._custom_clear_cache(cls, val))
+                cache_func.append(self._custom_clear_cache(ins, cls, val))
         ret = super().delete(using, keep_parents)
         for func in cache_func:
             func()
